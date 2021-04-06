32b24432a490f9997720a4071e4304cff170995c
NHau2803 <hau2803nch@gmail.com>
2021-04-06 15:17:23 +0700

    update-6-4

--- src/main/java/com/ex01/rest/activitylog/api/StudentActivityLogAPI.java
+++ src/main/java/com/ex01/rest/activitylog/api/StudentActivityLogAPI.java
@@ -1,7 +1,10 @@
 package com.ex01.rest.activitylog.api;
 
+import com.ex01.module.activitylogmodule.dto.ActivityLogCreateDTO;
+import com.ex01.module.activitylogmodule.dto.ActivityLogDTO;
 import com.ex01.module.activitylogmodule.model.ActivityLogModel;
 import com.ex01.module.activitylogmodule.service.ActivityLogService;
+import com.ex01.module.activitylogmodule.utils.domain.JsonResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -19,7 +22,7 @@ import java.util.List;
 
 @Api(tags = "StudentActivityLog")
 @RestController
-@RequestMapping("/ex01")
+@RequestMapping("/ex01/log")
 public class StudentActivityLogAPI {
 
     @Autowired
@@ -27,40 +30,40 @@ public class StudentActivityLogAPI {
 
     private Logger logger = LoggerFactory.getLogger(StudentActivityLogAPI.class);
 
-//    @ApiOperation(value = "This is an example of Rest API Endpoint")
-//    @ApiResponses(value = { @ApiResponse(code = 200, response = String.class, message = "Success") })
-//    @RequestMapping(value = "/search/{pageStart}/{pageSize}", method = RequestMethod.GET)
-//    public ResponseEntity<?> search(@PathVariable int pageStart, @PathVariable int pageSize){
-//        List<ActivityLogModel> results = new ArrayList<>();
-//        Page<ActivityLogModel> resultsIterator = activityLogService.search(pageStart, pageSize);
-//        JsonResponse jsonResponse = new JsonResponse(resultsIterator.getContent(), true);
-//        return new ResponseEntity<JsonResponse>(jsonResponse, HttpStatus.OK);
-//    }
-//
-//    @ApiOperation(value = "This is an example of Rest API Endpoint")
-//    @ApiResponses(value = { @ApiResponse(code = 200, response = String.class, message = "Success") })
-//    @RequestMapping(value = "/add", method = RequestMethod.POST)
-//    public ResponseEntity<?> add(@RequestBody(required=false) StudentInformationCreateDTO request){
-//        StudentInformationCreateDTO datas = activityLogService.add(request);
-//        JsonResponse jsonResponse = new JsonResponse(datas, true);
-//        return new ResponseEntity<JsonResponse>(jsonResponse, HttpStatus.CREATED);
-//    }
-//
-//    @ApiOperation(value = "This is an example of Rest API Endpoint")
-//    @ApiResponses(value = { @ApiResponse(code = 200, response = String.class, message = "Success") })
-//    @RequestMapping(value = "/update", method = RequestMethod.POST)
-//    public ResponseEntity<?> update(@RequestBody(required=false) StudentInformationDTO request){
-//        StudentInformationModel datas = activityLogService.update(request);
-//        JsonResponse jsonResponse = new JsonResponse(datas, true);
-//        return new ResponseEntity<JsonResponse>(jsonResponse, HttpStatus.OK);
-//    }
-//
-//    @ApiOperation(value = "This is an example of Rest API Endpoint")
-//    @ApiResponses(value = { @ApiResponse(code = 200, response = String.class, message = "Success") })
-//    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-//    public ResponseEntity<?> delete(@PathVariable String id){
-//        boolean success = activityLogService.delete(id);
-//        JsonResponse jsonResponse = new JsonResponse(success);
-//        return new ResponseEntity<JsonResponse>(jsonResponse, HttpStatus.OK);
-//    }
+    @ApiOperation(value = "This is an example of Rest API Endpoint")
+    @ApiResponses(value = { @ApiResponse(code = 200, response = String.class, message = "Success") })
+    @RequestMapping(value = "/search/{pageStart}/{pageSize}", method = RequestMethod.GET)
+    public ResponseEntity<?> search(@PathVariable int pageStart, @PathVariable int pageSize){
+        List<ActivityLogModel> results = new ArrayList<>();
+        Page<ActivityLogModel> resultsIterator = activityLogService.search(pageStart, pageSize);
+        JsonResponse jsonResponse = new JsonResponse(resultsIterator.getContent(), true);
+        return new ResponseEntity<JsonResponse>(jsonResponse, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "This is an example of Rest API Endpoint")
+    @ApiResponses(value = { @ApiResponse(code = 200, response = String.class, message = "Success") })
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ResponseEntity<?> add(@RequestBody(required=false) ActivityLogCreateDTO request){
+        ActivityLogCreateDTO data = activityLogService.add(request);
+        JsonResponse jsonResponse = new JsonResponse(data, true);
+        return new ResponseEntity<JsonResponse>(jsonResponse, HttpStatus.CREATED);
+    }
+
+    @ApiOperation(value = "This is an example of Rest API Endpoint")
+    @ApiResponses(value = { @ApiResponse(code = 200, response = String.class, message = "Success") })
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ResponseEntity<?> update(@RequestBody(required=false) ActivityLogDTO request){
+        ActivityLogDTO datas = activityLogService.update(request);
+        JsonResponse jsonResponse = new JsonResponse(datas, true);
+        return new ResponseEntity<JsonResponse>(jsonResponse, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "This is an example of Rest API Endpoint")
+    @ApiResponses(value = { @ApiResponse(code = 200, response = String.class, message = "Success") })
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable String id){
+        boolean success = activityLogService.delete(id);
+        JsonResponse jsonResponse = new JsonResponse(success);
+        return new ResponseEntity<JsonResponse>(jsonResponse, HttpStatus.OK);
+    }
 }
